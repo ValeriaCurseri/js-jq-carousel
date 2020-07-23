afb51b5a6fafa76c3c2eae45f38bf60de2c41bf3
Valeria Curseri <curserivaleria@gmail.com>
2020-07-23 14:25:38 +0200

    Attivazione link sulle duefrecce

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 // 1. attivo il click sulla freccia next
 
+$('.next').click(function(){
+    nextImage();
+});
+
+// 3. attivo il click sulla freccia prev
+
+$('.prev').click(function(){
+    prevImage();
+});
+
 // 2. scrivo la funzione per la freccia next
 // 2a. individuo l'immagine con la classe active con una variabile
 // 2b. tolgo la classe active
 // 2c. aggiungo la classe active all'elemento successivo
 
-// 3. scrivo la funzione per la freccia prev
-// 3a. individuo l'immagine con la classe active con una variabile
-// 3b. tolgo la classe active
-// 3c. aggiungo la classe active all'elemento precedente
+
+
+// 4. scrivo la funzione per la freccia prev
+// 4a. individuo l'immagine con la classe active con una variabile
+// 4b. tolgo la classe active
+// 4c. aggiungo la classe active all'elemento precedente
+
+// 5. implemento la funzione della freccia next con le istruzioni per i pallini
+// 5a. individuo il pallino con la classe active con una variabile
+// 5b. tolgo la classe active
+// 5c. aggiungo la classe active all'elemento successivo
+
+// 6. implemento la funzione della freccia prev con le istruzioni per i pallini
+// 6a. individuo il pallino con la classe active con una variabile
+// 6b. tolgo la classe active
+// 6c. aggiungo la classe active all'elemento precedente
