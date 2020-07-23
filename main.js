e8a6b209a444de8e3d5019cc544f02ff1eb23155
Valeria Curseri <curserivaleria@gmail.com>
2020-07-23 14:21:29 +0200

    File di base caricati con struttura file js

---
+++ main.js
@@ -0,0 +1,11 @@
+// 1. attivo il click sulla freccia next
+
+// 2. scrivo la funzione per la freccia next
+// 2a. individuo l'immagine con la classe active con una variabile
+// 2b. tolgo la classe active
+// 2c. aggiungo la classe active all'elemento successivo
+
+// 3. scrivo la funzione per la freccia prev
+// 3a. individuo l'immagine con la classe active con una variabile
+// 3b. tolgo la classe active
+// 3c. aggiungo la classe active all'elemento precedente
